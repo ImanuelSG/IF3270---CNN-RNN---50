01cd1760b80235fc32019c057b807318b60084d5
Imanuel Girsang <13522058@std.stei.itb.ac.id>
2025-05-12 21:28:29 +0700

    feat: add autodiff

---
+++ src/layers/cnn/dense.py
@@ -0,0 +1,16 @@
+from layer import Layer
+class Conv2D(Layer):
+    def __init__(self, fi):
+        self.filters = None
+        self.kernel_size = None
+        self.stride = None
+        self.padding = 
+        self.bias = None
+        super().__init__()
+
+    def forward(self, x):
+        # Implement the forward pass for 2D convolution
+
+        pass
+
+    def
--- src/layers/layer.py
+++ src/layers/layer.py
@@ -1,6 +1,9 @@
 class Layer:
     def forward(self, x):
         raise NotImplementedError("Forward pass not implemented.")
+    
+    def backward(self, x):
+        raise NotImplementedError("Backward pass not implemented.")
 
     def __call__(self, x):
         return self.forward(x)
